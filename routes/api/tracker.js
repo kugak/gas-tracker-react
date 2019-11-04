b4f4deaaf0c65cbc73330d8a02267fee12484328
Kuga Kugaraja <contact@kuga.ca>
2019-11-04 14:06:21 -0500

    save to db

--- routes/api/tracker.js
+++ routes/api/tracker.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const Tracker = require('../../models/Tracker');
 
 // @route GET api/tracker
 // @desc Get all values
@@ -21,19 +22,34 @@ router.post(
       .isEmpty(),
     check('endkm', 'Ending KM is required')
       .not()
-      .isEmpty(),
-    check('date', 'Date KM is required')
-      .not()
       .isEmpty()
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    console.log(req.body);
-    res.send('Tracker Route');
+    const { amount, litres, endkm, date } = req.body;
+
+    try {
+      console.log(req.body);
+      res.send('Added');
+
+      // create tracker instance
+      tracker = new Tracker({
+        amount,
+        litres,
+        endkm,
+        date
+      });
+
+      // save to db
+      await tracker.save();
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
